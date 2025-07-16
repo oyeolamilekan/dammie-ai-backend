8daf2f3644cb04300f9f5ebcdd87ae1b26e854ab
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2025-07-16 20:25:03 +0100

    chore: increased the number of supported currencies.

--- src/config/config.ts
+++ src/config/config.ts
@@ -40,7 +40,7 @@ const CONFIG = {
   PENDING: 'pending',
   FAILED: 'failed',
   SUCCESS: 'success',
-  SUPPORTED_CRYPTOS: ['trx'],
+  SUPPORTED_CRYPTOS: ['btc', 'usdt', 'usdc'],
   STATUS: ['pending', 'failed', 'success'],
   MAIN_ACCOUNT_ID: process.env.MAIN_ACCOUNT_ID as string,
   CURRENT_RATES: {
